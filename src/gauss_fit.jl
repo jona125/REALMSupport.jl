83281d54981d0b81cbe6cc747bbd369c1caf0183
Tim Holy <tim.holy@gmail.com>
2021-12-12 02:33:50 -0600

    Improve Gaussian fitting
    This makes several changes that should make Gaussian fits more robust:
    - parameters are initialized based on the input data (rather than fixed)
    - optimization uses automatic differentiation & Newton's method,
      allowing higher accuracy
    - optimization allows one to set options via keyword arguments.
      This is particularly useful for, e.g., adjusting convergence tolerance
        in cases where we expect a perfect fit.
    
    There are also important changes to the tests:
    - noise is centered around 0, rather than always being positive
      (the latter biases your fit of the amplitude)
    - accuracy criterion now depends on the magnitude of the noise

--- src/gauss_fit.jl
+++ src/gauss_fit.jl
@@ -1,7 +1,7 @@
 
 export gauss_line_fit
 
-function gaussclossure(X)
+function gaussclossure(X::AbstractVector)
 	function gaussloss(params)
 		a, σ = params[1], params[2]
 		x = a * [exp(-(t)^2/(2*σ^2)) for t in axes(X,1)]
@@ -10,11 +10,14 @@ function gaussclossure(X)
 	return gaussloss
 end
 
-function gauss_line_fit(X)
+function gauss_line_fit(X::AbstractVector; kwargs...)
 	f = gaussclossure(X)
-	params = [1e-8,0.5]
+	sX = sum(X)
+	σ² = sum(i^2 * X[i] for i in axes(X,1))/sX
+	σ  = sqrt(max(σ², oftype(σ², 1e-4)))  # σ >= 0.01 since we only have integer-scale resolution
+	params = [sX/(σ*sqrt(2π)), σ]
 
-	result = optimize(f, params, LBFGS())
+	result = optimize(f, params, Newton(), Optim.Options(; kwargs...); autodiff=:forward)
 	Optim.converged(result) || @warn "Optimization failed to converge"
 	return result
 	#return Optim.minimizer(result)
