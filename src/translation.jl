04a4fa2e15a323b5c40272c01d38a3e8e31aa873
Jonathan Chang <jona125@gmail.com>
2021-12-14 20:44:01 -0600

    Fix conflict.

--- src/translation.jl
+++ src/translation.jl
@@ -1,20 +1,22 @@
 
 export translate, translate_optim
 
-function translationclossure(mm, movement)
+function translationclossure(mm::AbstractArray{T,N}, movement) where {T,N}
     function translation_loss(matrix)
-        c = reshape(matrix, 2, 2) * movement
-        c .+= (size(mm, 1) + 1) / 2
+        dims = N - 1
+        c = reshape(matrix, dims, dims) * movement
+        #c .+= (size(mm,1)+1)/2
         loss = zero(eltype(mm))
-        for i in size(c, 2)
-            idx1, idx2 = c[1, i], c[2, i]
-            checkbounds(Bool, mm, idx1, idx2, i) || return typemax(loss)
-            loss += mm[idx1, idx2, i]
+        for (i, j) in enumerate(axes(mm, N))
+            idx = c[:, i]
+            checkbounds(Bool, mm, idx..., j) || return typemax(eltype(loss))
+            loss += mm(idx..., j)
         end
         return loss
     end
     return translation_loss
 end
+
 
 function translate(fixed, moving, maxshift, thresh = nothing)
     mm = RegisterMismatch.mismatch(fixed, moving, maxshift)
