f1962f4b9d5749ae10860a93f4e39103f58bf233
Jonathan Chang <jona125@gmail.com>
2021-12-14 19:53:59 -0600

    Fix Formatting.

--- src/BG_correct.jl
+++ src/BG_correct.jl
@@ -20,7 +20,7 @@ function BG_correct(img,filename,GRIN)
 		end
 	end
 	if(GRIN==1)
-		b_mean = zeros(x,y)
+        b_mean = zeros(x,y)
 		for i in 1:x
 			for j in 1:y
 				b_mean[i,j] = mean(filtered[i,j,:])
@@ -29,15 +29,14 @@ function BG_correct(img,filename,GRIN)
 		base = maximum(b_mean)		
 
 		@showprogress @sprintf("GRIN lens correction of Record %s...",filename) for t in 1:size(img,3)
-                	img2 = img1[:,:,t]
-	
-        	        for i in 1:x
-                	        for j in 1:y
-                        	        filtered[i,j,t] = filtered[i,j,t] - b_mean[i,j] + base
-					if (filtered[i,j,t] >= 1) filtered[i,j,t] = 1 end
-                        	end
-         	       end
-        	end
+            img2 = img1[:,:,t]
+            for i in 1:x
+                for j in 1:y
+                    filtered[i,j,t] = filtered[i,j,t] - b_mean[i,j] + base
+                    if (filtered[i,j,t] >= 1) filtered[i,j,t] = 1 end
+                end
+            end
+        end
 	end
 
 	img_save(filtered,"/home/jchang/image/result/",@sprintf("%s-b.tif",filename))
