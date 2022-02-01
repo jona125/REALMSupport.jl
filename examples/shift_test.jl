33a13b8d47ebc2cf76f61b831cff7211f376cb12
Jonathan Chang <jona125@gmail.com>
2022-02-01 03:11:51 -0600

    Fix julaformat

--- examples/shift_test.jl
+++ examples/shift_test.jl
@@ -1,28 +1,32 @@
 using REALMSupport
 using REALMSupport.RegisterCore: CenterIndexedArray
-using Optim,Images,RegisterMismatch
+using Optim, Images, RegisterMismatch
 
 filelist = readdir()
-filelist=filter(x->occursin(".tif",x),filelist)
-filelist=filter(x->occursin("1um",x),filelist)
+filelist = filter(x -> occursin(".tif", x), filelist)
+filelist = filter(x -> occursin("1um", x), filelist)
 
-mm = CenterIndexedArray(zeros(201,201,201,size(filelist,1)))
+mm = CenterIndexedArray(zeros(201, 201, 201, size(filelist, 1)))
 mm_r = []
 img_org = load("20211013_1um-x-0_1.tif")
 
 for (i, j) in enumerate(axes(mm, 4))
     img = load(filelist[i])
-    mm[:,:,:,j] = translate(img_org,img,(100,100,100))
-    push!(mm_r,register_translate(img_org,img,(100,100,100)))
+    mm[:, :, :, j] = translate(img_org, img, (100, 100, 100))
+    push!(mm_r, register_translate(img_org, img, (100, 100, 100)))
 end
 
 display(mm)
 
-move = [40 80 0 -40 -80 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 10 20 30 40 -10 -20 -30 -40]
+move = [
+    40 80 0 -40 -80 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 10 20 30 40 -10 -20 -30 -40
+]
 
 matrix = Float64[-0.17 0 -1.18 0 0 0 0.55 0 -0.01]
 
 result = translate_optim(mm, matrix, move; g_abstol = 1e-14)
 
-mat = reshape(Optim.minimizer(result),3,3)
+mat = reshape(Optim.minimizer(result), 3, 3)
 display(mat)
--- src/segmentbeads.jl
+++ src/segmentbeads.jl
@@ -1,9 +1,9 @@
 
 export beads_segment
 
-function beads_segment(img::AbstractArray,threshold)
+function beads_segment(img::AbstractArray, threshold)
     filtered = zeros(size(img))
-    
+
     m = mean(img)
     s = std(img)
     img[img.>m+3*s] .= 1
@@ -12,18 +12,18 @@ function beads_segment(img::AbstractArray,threshold)
     label = label_components(img)
     out = component_pixels(label)
     out_fil = filter(out) do item
-                length(item) >= threshold && length(item) <= 1000
-              end
+        length(item) >= threshold && length(item) <= 1000
+    end
 
-    @showprogress @sprintf("Filtering %s",filename) for vec in out_fil
+    @showprogress @sprintf("Filtering %s", filename) for vec in out_fil
         for coord in vec
             filtered[coord] = img[coord]
         end
     end
 
-    replace!(filtered, NaN=>0)
-    img3 = Gray.(convert(Array{N0f16},OffsetArrays.no_offset_view(filtered)))
-    img4 = Gray.(convert.(Normed{UInt16,16},img3))
+    replace!(filtered, NaN => 0)
+    img3 = Gray.(convert(Array{N0f16}, OffsetArrays.no_offset_view(filtered)))
+    img4 = Gray.(convert.(Normed{UInt16,16}, img3))
     return img4
 end
 
