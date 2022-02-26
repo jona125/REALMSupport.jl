9e02e644a135c9d47d7404400264e8d7f347bab5
Jonathan Chang <jona125@gmail.com>
2022-02-25 20:14:53 -0600

    JuliaFormatter to format files.

--- examples/beads_psf.jl
+++ examples/beads_psf.jl
@@ -14,62 +14,57 @@ print("Disk label: ")
 label = chomp(readline())
 
 print("Date: ")
-date=chomp(readline())
+date = chomp(readline())
 #date="20190315"
 @show date
 
-cd(@sprintf("/mnt/%s/jchang/%s/",label,date))
-files=readdir()
-filelist=filter(x->occursin(".imagine",x),files)
+cd(@sprintf("/mnt/%s/jchang/%s/", label, date))
+files = readdir()
+filelist = filter(x -> occursin(".imagine", x), files)
 @show filelist
 
 print("add scale bar(Y as 1, N as 0): ")
-r=chomp(readline())
-r=parse(Int,r)
+r = chomp(readline())
+r = parse(Int, r)
 
-l=0        
+l = 0
 if r == 1
-	print("The length of scale bar(in px): ")
-        l = chomp(readline())
-        l = parse(Int,l)
+    print("The length of scale bar(in px): ")
+    l = chomp(readline())
+    l = parse(Int, l)
 end
 
 print("Background Filename: ")
 BG_filename = chomp(readline())
 
 if BG_filename == ""
-	BG_filename = filelist[1][1:end-8]
-	BG_filename *= "_1"
+    BG_filename = filelist[1][1:end-8]
+    BG_filename *= "_1"
 end
 
 print("Save every file during process (1 as Yes, 0 as No): ")
 Save = chomp(readline())
 Save = (Save == "1")
 
-for k in 1:size(filelist,1)
-	filename=filelist[k][1:end-8]
-	exp = load(@sprintf("%s.imagine", filename))
+for k = 1:size(filelist, 1)
+    filename = filelist[k][1:end-8]
+    exp = load(@sprintf("%s.imagine", filename))
 
-	transfertotif(exp,filename,r,l) # save .imagine into .tif
-	files=readdir()
-	framelist=filter(x->occursin(".tif",x),files)
-	framelist=filter(x->occursin(@sprintf("%s",filename),x),framelist)
-	
-	for i in 1:size(framelist,1)
-		filename=framelist[i][1:end-4]
-		exp = load(@sprintf("%s.tif", filename))
+    transfertotif(exp, filename, r, l) # save .imagine into .tif
+    files = readdir()
+    framelist = filter(x -> occursin(".tif", x), files)
+    framelist = filter(x -> occursin(@sprintf("%s", filename), x), framelist)
 
-		#img = BG_subtraction(exp,BG_filename,filename,Save)# filter image with background signal
+    for i = 1:size(framelist, 1)
+        filename = framelist[i][1:end-4]
+        exp = load(@sprintf("%s.tif", filename))
+
+        #img = BG_subtraction(exp,BG_filename,filename,Save)# filter image with background signal
         img = convert(Array{N0f16}, exp)
-        img = beads_segment(img, THRESHOLD,10000,3)
+        img = beads_segment(img, THRESHOLD, 10000, 3)
         #psf_analyze(img,filename)
-        img3 = Gray.(convert(Array{N0f16},OffsetArrays.no_offset_view(img)))
-        img4 = Gray.(convert.(Normed{UInt16,16},img3))
-        img_save(img,"/home/jchang/image/result/",@sprintf("%s-fi.tif",filename))
-	end
+        img3 = Gray.(convert(Array{N0f16}, OffsetArrays.no_offset_view(img)))
+        img4 = Gray.(convert.(Normed{UInt16,16}, img3))
+        img_save(img, "/home/jchang/image/result/", @sprintf("%s-fi.tif", filename))
+    end
 end
-
-
-
-
-
--- examples/shift_test.jl
+++ examples/shift_test.jl
@@ -1,22 +1,26 @@
 using REALMSupport
 using REALMSupport.RegisterCore: CenterIndexedArray
-using Optim,Images,RegisterMismatch
+using Optim, Images, RegisterMismatch
 
 filelist = readdir()
-filelist=filter(x->occursin("-fi.tif",x),filelist)
-filelist=filter(x->occursin("20220221_1um",x),filelist)
+filelist = filter(x -> occursin("-fi.tif", x), filelist)
+filelist = filter(x -> occursin("20220221_1um", x), filelist)
 
-mm = CenterIndexedArray(zeros(201,201,201,size(filelist,1)))
+mm = CenterIndexedArray(zeros(201, 201, 201, size(filelist, 1)))
 mm_r = []
 img_org = load("20220221_1um_x-0_1_1-fi.tif")
 
 for (i, j) in enumerate(axes(mm, 4))
     img = load(filelist[i])
-    mm[:,:,:,j] = translate(img_org,img,(100,100,100))
-    push!(mm_r,register_translate(img_org,img,(100,100,100)))
+    mm[:, :, :, j] = translate(img_org, img, (100, 100, 100))
+    push!(mm_r, register_translate(img_org, img, (100, 100, 100)))
 end
 
-move = [20 20 40 40 0 0 -20 -20 -40 -40 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 20 20 40 40 0 -20 -20 -40 -40 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 20 20 40 40 0 0 -20 -20 -40 -40]
+move = [
+    20 20 40 40 0 0 -20 -20 -40 -40 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0 20 20 40 40 0 -20 -20 -40 -40 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 20 20 40 40 0 0 -20 -20 -40 -40
+]
 
 
 #move = [20 20 20 40 40 40 0 0 0 -20 -20 -20 -40 -40 -40 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 20 20 20 40 40 40 0 0 0 -20 -20 -20 -40 -40 -40 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 20 20 20 40 40 40 0 0 0 -20 -20 -20 -40 -40 -40]
@@ -26,6 +30,6 @@ matrix = Float64[-0.01 0 -1.84 0 1.56 0 1.198 0 -0.39]
 
 result = translate_optim(mm, matrix, move; g_abstol = 1e-14)
 
-mat = reshape(Optim.minimizer(result),3,3)
+mat = reshape(Optim.minimizer(result), 3, 3)
 display(mat)
-display((mat*move)')
+display((mat * move)')
