fd1739358d909c22fd95fcff579ca224cdfc0d98
Tim Holy <tim.holy@gmail.com>
2023-12-15 16:42:13 -0600

    Add record_points (#39)
    This allows users to click on the image and mark points.
    This permits beads or other objects to be identified by hand.

--- src/REALMSupport.jl
+++ src/REALMSupport.jl
@@ -8,6 +8,8 @@ using RegisterMismatch.RegisterCore
 
 export grid_resolution, gauss_line_fit, img_save, beads_segment, component_pixels
 export centerofmass, findmid, translate, translate_optim, transfertotif, timeseries
+export record_points
+
 include("grid_fun.jl")
 include("gauss_fit.jl")
 include("img_save.jl")
@@ -16,5 +18,6 @@ include("translation.jl")
 include("segmentbeads.jl")
 include("s_save_image.jl")
 include("time.jl")
+include("click_gui.jl")
 
 end
