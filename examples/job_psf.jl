8b0f87d068d01f58038525e18f236c2986f9b0c3
Jonathan Che-Pin Chang <36974316+jona125@users.noreply.github.com>
2024-04-16 12:27:55 -0500

    Update psf script. Using job_psf.jl as the job submission script. (#37)
    * Update psf script. Using job_psf.jl as the job submission script.
    
    * Merge CompatHelper (#38)
    
    * CompatHelper: bump compat for ImageMorphology to 0.4, (keep existing compat) (#34)
    
    Co-authored-by: CompatHelper Julia <compathelper_noreply@julialang.org>
    
    * CompatHelper: bump compat for Interpolations to 0.14, (keep existing compat) (#35)
    
    Co-authored-by: CompatHelper Julia <compathelper_noreply@julialang.org>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: CompatHelper Julia <compathelper_noreply@julialang.org>
    
    * Add record_points (#39)
    
    This allows users to click on the image and mark points.
    This permits beads or other objects to be identified by hand.
    
    * merge click record and fix CI/CD problem.
    
    * Fix CI gtk problem.
    
    * Fix conflict.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: CompatHelper Julia <compathelper_noreply@julialang.org>
    Co-authored-by: Tim Holy <tim.holy@gmail.com>

---
+++ examples/job_psf.jl
@@ -0,0 +1,40 @@
+import Pkg
+Pkg.activate("/storage1/fs1/holy/Active/jchang/.julia/dev/REALMSupport")
+
+
+using Images, StaticArrays, LinearAlgebra, Statistics, JLD2
+using FileIO, ProgressMeter, ImageSegmentation
+using REALMSupport
+include("beadstest.jl")
+include("pipeline2_3d_trans.jl")
+
+cd(ARGS[1])
+
+files = readdir()
+filelist = filter(x->occursin(".imagine",x),files)
+
+for k in 1:size(filelist,1)
+
+    exp = load(filelist[k])
+    exp = exp.data.data
+    x_ = []
+    y_ = []
+    z_ = []
+    for i in 1:size(exp,4)
+        img = exp[:,:,:,i]
+        img = convert(Array{Float64}, img)
+        for ix in CartesianIndices(img)
+            img[ix[1],ix[2],:] .-= mean(img[ix[1],ix[2],:])
+        end
+        img .-= minimum(img)
+        img = normal(img)
+        img = convert(Array{N0f16}, img)
+        x, y, z = beadstest(img, @sprintf("%s_%d", filelist[k][1:end-8], i), pwd(),true) # Using beadtest function from beadtest.jl
+        push!(x_, x)
+        push!(y_, y)
+        push!(z_, z)
+        img = nothing
+    end
+    jldsave(@sprintf("%s_psf.jld2",filelist[k][1:end-8]);x_,y_,z_)
+end
+
--- examples/pipeline2_3d_trans.jl
+++ examples/pipeline2_3d_trans.jl
@@ -1,21 +1,40 @@
-using Images, ImageView
+using Images, Statistics
 using CoordinateTransformations, Rotations, OffsetArrays
 
 function normal(img)
-    max_num = maximum(img)
-    return img ./ max_num
+   max_num = maximum(img)
+   return img ./ max_num
 end
 
-function pipeline2(img)
-    M = [
-        0.000163234 4.73411e-5 0.860779
-        0.0050956 0.637071 0.0214116
-        0.684893 -0.00907678 0.00739394
-    ]
-    v = [0.0, 0.0, 0.0]
+function img_sub(img)
+    for i in (1:size(img)[3]).+axes(img)[3].offset
+        img[:,:,i] .-= mean(img[:,:,i])
+    end
+    return img
+end
 
-    rot = AffineMap(M * [1 0 0; 0 1 0; 0 0 1], v)
-    img_r = warp(img, rot)
+function pipeline2(img;z_set = 1 ,x_angle = 0.0 ,y_angle = 0.0 ,z_angle = 0.0)
+    v = [0.0, 0.0, 0.0]
+    tr = zeros(3,3)
+    [tr[x,x] = 1.0 for x in 1:3]
+    tr[2:3,2:3] = RotMatrix(z_angle)
+    tr[1:2,1:2] = RotMatrix(y_angle)
+    (tr[3,3],tr[1,3],tr[3,1],tr[1,1]) = RotMatrix(x_angle)
+    if z_set == 1
+        M = [-9.40453e-6  -0.00145012  1.19443
+              3.22872e-5   1.37811     0.000187239
+              -1.79544     -0.0268617   0.0203941]
+        rot = AffineMap(M*tr,v)
+    else
+        M = [-9.63642e-7  -0.000522643   0.831357
+              5.66957e-6   1.56471      -0.00101736
+              -1.82755     -0.00239608   -0.00096341]
+        rot = AffineMap(M*tr,v)
+    end
+        
+    img_r = warp(img,rot)
     img_nor = normal(img_r)
+    img_nor = permutedims(img_nor, [2,3,1])
+    img_nor = img_sub(img_nor)
     return img_nor
 end
