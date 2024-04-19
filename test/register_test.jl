15df29250140070a26c1cb7ae14fded226f73624
jona125 <36974316+jona125@users.noreply.github.com>
2024-04-19 17:53:23 -0500

    Update test case.

--- test/register_test.jl
+++ test/register_test.jl
@@ -16,7 +16,7 @@ using RegisterMismatch
     mm[:, :, 1] = translate(A, C, (5, 5))
     mm_r[:, 3] .= Tuple(register_translate(A, C, (5, 5)))
 
-    move = [0 1 2; 0 0 0]
+    move = [0 2 4; 0 0 0]
     matrix = Float64.(zeros(4))
     result = translate_optim(mm, matrix, move; g_abstol = 1e-14)
 
@@ -35,25 +35,25 @@ using RegisterMismatch
     F = outer[3:16, 7:12, 11:15]
     G = outer[3:16, 7:12, 12:16]
 
-    mm = CenterIndexedArray(zeros(11, 11, 11, 7))
+    mm = CenterIndexedArray(zeros(15, 15, 15, 7))
     mm_r = zeros(3, 7)
-    mm[:, :, :, -3] = translate(A, A, (5, 5, 5))
-    mm_r[:, 1] .= Tuple(register_translate(A, A, (5, 5, 5)))
-    mm[:, :, :, -2] = translate(A, B, (5, 5, 5))
-    mm_r[:, 2] .= Tuple(register_translate(A, B, (5, 5, 5)))
-    mm[:, :, :, -1] = translate(A, C, (5, 5, 5))
-    mm_r[:, 3] .= Tuple(register_translate(A, C, (5, 5, 5)))
-    mm[:, :, :, 0] = translate(A, D, (5, 5, 5))
-    mm_r[:, 4] .= Tuple(register_translate(A, D, (5, 5, 5)))
-    mm[:, :, :, 1] = translate(A, E, (5, 5, 5))
-    mm_r[:, 5] .= Tuple(register_translate(A, E, (5, 5, 5)))
-    mm[:, :, :, 2] = translate(A, F, (5, 5, 5))
-    mm_r[:, 6] .= Tuple(register_translate(A, F, (5, 5, 5)))
-    mm[:, :, :, 3] = translate(A, G, (5, 5, 5))
-    mm_r[:, 7] .= Tuple(register_translate(A, G, (5, 5, 5)))
+    mm[:, :, :, -3] = translate(A, A, (7, 7, 7))
+    mm_r[:, 1] .= Tuple(register_translate(A, A, (7, 7, 7)))
+    mm[:, :, :, -2] = translate(A, B, (7, 7, 7))
+    mm_r[:, 2] .= Tuple(register_translate(A, B, (7, 7, 7)))
+    mm[:, :, :, -1] = translate(A, C, (7, 7, 7))
+    mm_r[:, 3] .= Tuple(register_translate(A, C, (7, 7, 7)))
+    mm[:, :, :, 0] = translate(A, D, (7, 7, 7))
+    mm_r[:, 4] .= Tuple(register_translate(A, D, (7, 7, 7)))
+    mm[:, :, :, 1] = translate(A, E, (7, 7, 7))
+    mm_r[:, 5] .= Tuple(register_translate(A, E, (7, 7, 7)))
+    mm[:, :, :, 2] = translate(A, F, (7, 7, 7))
+    mm_r[:, 6] .= Tuple(register_translate(A, F, (7, 7, 7)))
+    mm[:, :, :, 3] = translate(A, G, (7, 7, 7))
+    mm_r[:, 7] .= Tuple(register_translate(A, G, (7, 7, 7)))
 
 
-    move = [0 1 2 0 0 0 0; 0 0 0 1 2 0 0; 0 0 0 0 0 1 2]
+    move = [0 1 2 0 0 0 0; 0 0 0 2 4 0 0; 0 0 0 0 0 0.5 1]
     matrix = Float64.(zeros(9))
     result = translate_optim(mm, matrix, move; g_abstol = 1e-14)
 
