57eb56e4b75926cae62afacbd9e918c1c389a5fc
jona125 <36974316+jona125@users.noreply.github.com>
2024-04-19 05:51:02 -0500

    Fix translation matrix package and test case.

--- test/register_test.jl
+++ test/register_test.jl
@@ -1,4 +1,5 @@
 using REALMSupport.RegisterCore: CenterIndexedArray
+using RegisterMismatch
 
 @testset "Translation function test" begin
     outer = reshape(1:480, 24, 20)
@@ -7,36 +8,56 @@ using REALMSupport.RegisterCore: CenterIndexedArray
     C = outer[5:20, 1:16]
 
     mm = CenterIndexedArray(zeros(11, 11, 3))
+    mm_r = zeros(2,3)
     mm[:, :, -1] = translate(A, A, (5, 5))
+    mm_r[:, 1] .= Tuple(register_translate(A, A, (5, 5)))
     mm[:, :, 0] = translate(A, B, (5, 5))
+    mm_r[:, 2] .= Tuple(register_translate(A, B, (5, 5)))
     mm[:, :, 1] = translate(A, C, (5, 5))
+    mm_r[:, 3] .= Tuple(register_translate(A, C, (5, 5)))
 
     move = [0 1 2; 0 0 0]
-    matrix = Float64[1 0 0 1]
+    matrix = Float64.(zeros(4))
     result = translate_optim(mm, matrix, move; g_abstol = 1e-14)
 
     @test minimum(result) < 1e-3
+    @test reshape(Optim.minimizer(result), 2, 2) * move ≈ mm_r rtol = 1e-4
     #display(reshape(Optim.minimizer(result), 2, 2))
+
 
     #3d translation test
     outer = reshape(1:9600, 24, 20, 20)
     A = outer[3:16, 7:12, 10:14]
-    B = outer[5:18, 6:11, 10:14]
-    C = outer[7:20, 5:10, 10:14]
+    B = outer[4:17, 7:12, 10:14]
+    C = outer[5:18, 7:12, 10:14]
     D = outer[3:16, 6:11, 10:14]
     E = outer[3:16, 5:10, 10:14]
+    F = outer[3:16, 7:12, 11:15]
+    G = outer[3:16, 7:12, 12:16]
 
-    mm = CenterIndexedArray(zeros(7, 7, 3, 5))
-    mm[:, :, :, -2] = translate(A, A, (3, 3, 1))
-    mm[:, :, :, -1] = translate(A, B, (3, 3, 1))
-    mm[:, :, :, 0] = translate(A, C, (3, 3, 1))
-    mm[:, :, :, 1] = translate(A, D, (3, 3, 1))
-    mm[:, :, :, 2] = translate(A, E, (3, 3, 1))
+    mm = CenterIndexedArray(zeros(11, 11, 11, 7))
+    mm_r = zeros(3, 7)
+    mm[:, :, :, -3] = translate(A, A, (5, 5, 5))
+    mm_r[:, 1] .= Tuple(register_translate(A, A, (5, 5, 5)))
+    mm[:, :, :, -2] = translate(A, B, (5, 5, 5))
+    mm_r[:, 2] .= Tuple(register_translate(A, B, (5, 5, 5)))
+    mm[:, :, :, -1] = translate(A, C, (5, 5, 5))
+    mm_r[:, 3] .= Tuple(register_translate(A, C, (5, 5, 5)))
+    mm[:, :, :, 0] = translate(A, D, (5, 5, 5))
+    mm_r[:, 4] .= Tuple(register_translate(A, D, (5, 5, 5)))
+    mm[:, :, :, 1] = translate(A, E, (5, 5, 5))
+    mm_r[:, 5] .= Tuple(register_translate(A, E, (5, 5, 5)))
+    mm[:, :, :, 2] = translate(A, F, (5, 5, 5))
+    mm_r[:, 6] .= Tuple(register_translate(A, F, (5, 5, 5)))
+    mm[:, :, :, 3] = translate(A, G, (5, 5, 5))
+    mm_r[:, 7] .= Tuple(register_translate(A, G, (5, 5, 5)))
 
-    move = [0 1 2 0 0; 0 0 0 1 2; 0 0 0 0 0]
-    matrix = Float64[1 1 0 1 1 0 1 1 0]
+
+    move = [0 1 2 0 0 0 0; 0 0 0 1 2 0 0; 0 0 0 0 0 1 2]
+    matrix = Float64.(zeros(9))
     result = translate_optim(mm, matrix, move; g_abstol = 1e-14)
 
     @test minimum(result) < 1e-3
+    @test reshape(Optim.minimizer(result), 3, 3) * move ≈ mm_r rtol = 0.5
     #display(reshape(Optim.minimizer(result), 3, 3))
 end
