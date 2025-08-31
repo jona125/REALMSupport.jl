02771c1aa9d5cf2660c5cabc1c5e82634d594c92
Chang <cchang@compute1-client-1.ris.wustl.edu>
2025-08-31 17:56:41 -0500

    update pipeline code

--- examples/pipeline2_3d_trans.jl
+++ examples/pipeline2_3d_trans.jl
@@ -14,7 +14,7 @@ function img_sub(img)
     return img
 end
 
-function pipeline2(img; z_set = 1, x_angle = 0.0, y_angle = 0.0, z_angle = 0.0)
+function pipeline2(img;x_angle = 0.0, y_angle = 0.0, z_angle = 0.0)
     v = [0.0, 0.0, 0.0]
     tr = zeros(3, 3)
     [tr[x, x] = 4 for x = 1:3]
@@ -23,28 +23,12 @@ function pipeline2(img; z_set = 1, x_angle = 0.0, y_angle = 0.0, z_angle = 0.0)
     (tr[3, 3], tr[1, 3], tr[3, 1], tr[1, 1]) = RotMatrix(x_angle)
     tr[3,3] *= 4
     tr[2,2] *= 4
-    if z_set == 1
-        M = [
-            -1.16486e-10 1.96222e-7 -1.66666
-            -1.67079e-9 1.4 8.84353e-8
-            1.19999 2.7493e-6 -7.6324e-7
-        ]
-        rot = AffineMap(M * tr, v)
-    elseif z_set == 2
-        M = [
-            0.000163234 4.73411e-5 0.860779
-            0.0050956 0.637071 0.0214116
-            0.684893 -0.00907678 0.00739394
-        ]
-        rot = AffineMap(M * tr, v)
-    else
-        M = [
-            -0.4375 0.0 1.25
-            0.328125 -1.25 -0.9375
-            0.25 0.0 0.0
-        ]
-        rot = AffineMap(M * tr, v)
-    end
+    M = [
+        -0.4375 0.0 1.25
+        0.328125 -1.25 -0.9375
+        0.25 0.0 0.0
+    ]
+    rot = AffineMap(M * tr, v)
 
     img_r = warp(img, rot)
     img_nor = normal(img_r)
